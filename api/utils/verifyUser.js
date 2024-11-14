cc9b4df7cae809da07c1f20d5ef69cd281be36a7
myroslavkosyk81 <80488038+myroslavkosyk81@users.noreply.github.com>
2024-11-14 17:45:06 +0200

    Update verifyUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,9 @@ import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    if(!token) return next(errorHandler(401, 'Unauthorized'));
+    console.log('token', token)
+    console.log(req)
+    if(!token) return next(errorHandler(401, 'Unauthorized222'));
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return next(errorHandler(403, 'Forbiden'))
